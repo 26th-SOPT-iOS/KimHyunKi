50e76b17432a6f9f70cddbe28cd3ba59e4eee074
MyunDev <ygj01069@naver.com>
2020-05-03 19:30:16 +0900

    2주차 로그인 과제

---
+++ Week2/iOS_SecondWeek_Assignment_Login/iOS_SecondWeek_Assignment_Login/StretchHeaderViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StretchHeaderViewController.swift
+//  iOS_SecondWeek_Assignment_Login
+//
+//  Created by 김현기 on 2020/05/02.
+//  Copyright © 2020 HyunKi Kim. All rights reserved.
+//
+
+import UIKit
+
+class StretchHeaderViewController: UIViewController {
+
+    @IBOutlet var contentView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
