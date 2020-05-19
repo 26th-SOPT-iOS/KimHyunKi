70feca7237740ebfc04dc9822e17cf783164dee4
MyunDev <ygj01069@naver.com>
2020-05-20 01:43:05 +0900

    4주차 과제 진행중

--- Week4/iOS_4thWeek_Assignment/iOS_4thWeek_Assignment/Resource/APIConstants.swift
+++ Week4/iOS_4thWeek_Assignment/iOS_4thWeek_Assignment/Resource/APIConstants.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-struct APIConstants{
+struct APIConstants {
     static let baseURL = "http://13.209.144.115:3333"
     static let signinURL = APIConstants.baseURL + "/user/signin"
     static let signupURL = APIConstants.baseURL + "/user/signup"
---
+++ Week4/iOS_4thWeek_Assignment/iOS_4thWeek_Assignment/ViewControllers/SignupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignupViewController.swift
+//  iOS_4thWeek_Assignment
+//
+//  Created by 김현기 on 2020/05/20.
+//  Copyright © 2020 HyunKi Kim. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func goToMain(_ sender: Any) {
+    
+    self.navigationController?.popToRootViewController(animated: true)
+    
+    }
+    
+    @IBAction func singup(_ sender: Any) {
+    
+    
+    }
+    
+}
